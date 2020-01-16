29ef633d4403185b5c6faec4a54e4b7c94fd1a7e
Daniel Lufcy <ddlufcy@gmail.com>
2020-01-16 15:51:31 -0500

    search function

--- app.js
+++ app.js
@@ -126,6 +126,37 @@ document.querySelector('#application-list').addEventListener('click', function (
     ui.showAlert('Job Removed', 'success');
     e.preventDefault();
 });
+//Filter function
+function filterFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("filterInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("mainTable");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+
+
+
+
+
+
+
+
+
 
 // //Event Listener Job Status
 // let jobStatus = document.querySelector('#application-list').addEventListener('dblclick', function (e) {
